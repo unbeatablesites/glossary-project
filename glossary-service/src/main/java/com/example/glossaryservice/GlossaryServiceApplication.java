9ad2d4ee0d9a9d60277e7a34a196f4101f35d119
DevAaron <devaaronlee@gmail.com>
2019-10-04 15:14:14 -0400

    initial

---
+++ glossary-service/src/main/java/com/example/glossaryservice/GlossaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.glossaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossaryServiceApplication.class, args);
+	}
+
+}
---
+++ glossary-service/src/test/java/com/example/glossaryservice/GlossaryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.glossaryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlossaryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
